58ec111a445a47414e0764556b05e6233d54990e
gpugobrrr <aslansayfimehr@googlemail.com>
2026-08-09 18:16:51 +0100

    Ignore ontology review build artifacts

--- apps/ontology-review/.next/dev/server/app/api/answers/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/answers/route.js")
-R.c("server/chunks/node_modules_0lggk09._.js")
-R.c("server/chunks/[root-of-the-server]__14u_kt3._.js")
-R.c("server/chunks/_next-internal_server_app_api_answers_route_actions_1kwlzs1.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/answers/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/answers/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- apps/ontology-review/.next/server/chunks/_next-internal_server_app_api_answers_route_actions_1kwlzs1.js
+++
@@ -1,3 +0,0 @@
-module.exports=[41780,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_answers_route_actions_1kwlzs1.js.map
